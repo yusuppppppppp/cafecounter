4ccb9ce9b51261f3062793325a93f524c8b52c02
yusuppppppppp <gondetgendat@gmail.com>
2026-07-22 23:00:39 +0700

    feat(api): implement graceful shutdown

--- apps/api/internal/app/app.go
+++ apps/api/internal/app/app.go
@@ -2,6 +2,9 @@ package app
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/yusuppppppppp/cafecounter/apps/api/configs"
@@ -29,5 +32,28 @@ func New(cfg *configs.Config) *App {
 func (a *App) Start() {
 	log.Printf("%s running on http://%s", a.config.AppName, a.config.Addr)
 
-	log.Fatal(a.server.Listen(a.config.Addr))
+	go func() {
+		if err := a.server.Listen(a.config.Addr); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(
+		quit,
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
+	<-quit
+
+	log.Println("Shutdown signal received...")
+
+	if err := a.server.Shutdown(); err != nil {
+		log.Printf("Failed to shutdown server: %v", err)
+		return
+	}
+
+	log.Println("Server stopped gracefully")
 }
