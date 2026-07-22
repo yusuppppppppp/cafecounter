477a429cc8a806536c3af57f4a3cad188cdc935c
yusuppppppppp <gondetgendat@gmail.com>
2026-07-22 22:00:43 +0700

    feat(api): configure request logger

--- apps/api/internal/middleware/middleware.go
+++ apps/api/internal/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
@@ -9,4 +10,9 @@ import (
 func Register(app *fiber.App) {
 	app.Use(recover.New())
 	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		TimeFormat: "15:04:05",
+		TimeZone:   "Asia/Jakarta",
+		Format:     "[${time}] ${status} | ${latency} | ${method} ${path}\n",
+	}))
 }
